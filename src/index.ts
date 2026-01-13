22295d26580979599e196a99e91dc06daf46e418
Mobminer69 <lamichhanesudhir9@gmail.com>
2026-01-13 13:04:28 +0545

    Add CORS support for HTTP methods

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export default {
 
     const corsHeaders = {
       "Access-Control-Allow-Origin": "*",
+       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type",
     };
 
